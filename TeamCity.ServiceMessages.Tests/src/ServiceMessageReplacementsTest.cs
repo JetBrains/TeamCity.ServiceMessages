619b70d260a0df4b00d585cdebeb0cf009a5b55a
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-11-09 19:18:02 +0100

    speenup encode/decode strings

---
+++ TeamCity.ServiceMessages.Tests/src/ServiceMessageReplacementsTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace JetBrains.TeamCity.ServiceMessages.Tests
+{
+  [TestFixture]
+  public class ServiceMessageReplacementsTest
+  {
+    [Test]
+    public void TestEncode_0()
+    {
+      Assert.AreEqual("", ServiceMessageReplacements.Encode(""));
+    }
+
+    [Test]
+    public void TestEncode_1()
+    {
+      Assert.AreEqual("a", ServiceMessageReplacements.Encode("a"));
+    }
+
+    [Test]
+    public void TestEncode_1_special()
+    {
+      Assert.AreEqual("|n", ServiceMessageReplacements.Encode("\n"));
+      Assert.AreEqual("|r", ServiceMessageReplacements.Encode("\r"));
+      Assert.AreEqual("|]", ServiceMessageReplacements.Encode("]"));
+      Assert.AreEqual("|'", ServiceMessageReplacements.Encode("'"));
+      Assert.AreEqual("||", ServiceMessageReplacements.Encode("|"));
+      Assert.AreEqual("|x", ServiceMessageReplacements.Encode("\u0085"));
+      Assert.AreEqual("|l", ServiceMessageReplacements.Encode("\u2028"));
+      Assert.AreEqual("|p", ServiceMessageReplacements.Encode("\u2029"));
+    }
+
+    [Test]
+    public void TestDecode_0()
+    {
+      Assert.AreEqual("", ServiceMessageReplacements.Decode(""));
+    }
+
+    [Test]
+    public void TestDecode_1()
+    {
+      Assert.AreEqual("a", ServiceMessageReplacements.Decode("a"));
+    }
+
+    [Test]
+    public void TestDecode_1_special()
+    {
+      Assert.AreEqual(ServiceMessageReplacements.Decode("|n"), "\n");
+      Assert.AreEqual(ServiceMessageReplacements.Decode("|r"), "\r");
+      Assert.AreEqual(ServiceMessageReplacements.Decode("|]"), "]");
+      Assert.AreEqual(ServiceMessageReplacements.Decode("|'"), "'");
+      Assert.AreEqual(ServiceMessageReplacements.Decode("||"), "|");
+      Assert.AreEqual(ServiceMessageReplacements.Decode("|x"), "\u0085");
+      Assert.AreEqual(ServiceMessageReplacements.Decode("|l"), "\u2028");
+      Assert.AreEqual(ServiceMessageReplacements.Decode("|p"), "\u2029");
+    }
+  }
+}
--- TeamCity.ServiceMessages/src/ServiceMessageReplacements.cs
+++ TeamCity.ServiceMessages/src/ServiceMessageReplacements.cs
@@ -1,16 +1,33 @@
-using System.Linq;
+using System;
+using System.Text;
 using JetBrains.TeamCity.ServiceMessages.Annotations;
 
 namespace JetBrains.TeamCity.ServiceMessages
 {
-  internal static class ServiceMessageReplacements
+  public class ServiceMessageReplacements
   {
     /// <summary>
     /// Performs TeamCity-format escaping of a string.
     /// </summary>
     public static string Encode([NotNull] string value)
     {
-      return Replacements.Aggregate(value, (val, rep) => val.Replace(rep.From, rep.To));
+      var sb = new StringBuilder(value.Length*2);
+      foreach (var ch in value)
+      {
+        switch(ch)
+        {
+          case '|'      : sb.Append("||"); break;  //
+          case '\''     : sb.Append("|'"); break;  //
+          case '\n'     : sb.Append("|n"); break;  //
+          case '\r'     : sb.Append("|r"); break;  //
+          case ']'      : sb.Append("|]"); break;  //
+          case '\u0085' : sb.Append("|x"); break;  //\u0085 (next line)=>|x
+          case '\u2028' : sb.Append("|l"); break;  //\u2028 (line separator)=>|l
+          case '\u2029' : sb.Append("|p"); break;  //
+          default:        sb.Append(ch); break;
+        }        
+      }
+      return sb.ToString();
     }
 
     /// <summary>
@@ -18,30 +35,35 @@ namespace JetBrains.TeamCity.ServiceMessages
     /// </summary>
     public static string Decode([NotNull] string value)
     {
-      return Replacements.Reverse().Aggregate(value, (val, rep) => val.Replace(rep.To, rep.From));
-    }
-
-    private static readonly EscapeData[] Replacements = {
-                                                          new EscapeData("|", "||"),
-                                                          new EscapeData("\'", "|'"),
-                                                          new EscapeData("\n", "|n"),
-                                                          new EscapeData("\r", "|r"),
-                                                          new EscapeData("]", "|]"),
-                                                          new EscapeData("\u0085", "|x"), //\u0085 (next line)=>|x
-                                                          new EscapeData("\u2028", "|l"),//\u2028 (line separator)=>|l
-                                                          new EscapeData("\u2029", "|p"),//\u2028 (line separator)=>|l
-                                                        };
-
-    private struct EscapeData
-    {
-      public readonly string From;
-      public readonly string To;
-
-      public EscapeData(string @from, string to)
+      var sb = new StringBuilder(value.Length);
+      var buff = value.ToCharArray();
+      int i = 0;
+      for (; i < buff.Length-1; i++)
       {
-        From = @from;
-        To = to;
+        var ch = buff[i];
+        if (ch != '|')
+        {
+          sb.Append(ch);
+          continue;
+        }
+
+        ch = buff[++i];
+        switch (ch)
+        {
+          case '|': sb.Append('|'); break;   //
+          case '\'': sb.Append('\''); break;  //
+          case 'n': sb.Append('\n'); break;  //
+          case 'r': sb.Append('\r'); break;  //
+          case ']': sb.Append(']'); break;  //
+          case 'x': sb.Append('\u0085'); break; //\u0085 (next line)=>|x
+          case 'l': sb.Append('\u2028'); break;//\u2028 (line separator)=>|l
+          case 'p': sb.Append('\u2029'); break; //
+          default:
+            throw new ArgumentException("Unexpected escape sequence \"{0}\".", "|" + ch);
+        }
       }
+      if (i < buff.Length) sb.Append(buff[i]);      
+      return sb.ToString();
     }
   }
 }
