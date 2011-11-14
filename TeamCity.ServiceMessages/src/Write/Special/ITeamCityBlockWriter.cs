d2573786f429cb6aab84d665b9764c5f145fb07d
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-11-14 16:47:51 +0100

    add more messages

--- TeamCity.ServiceMessages/src/Write/Special/ITeamCityBlockWriter.cs
+++ TeamCity.ServiceMessages/src/Write/Special/ITeamCityBlockWriter.cs
@@ -1,4 +1,5 @@
 using System;
+using JetBrains.TeamCity.ServiceMessages.Annotations;
 
 namespace JetBrains.TeamCity.ServiceMessages.Write.Special
 {
@@ -16,6 +17,53 @@ namespace JetBrains.TeamCity.ServiceMessages.Write.Special
     /// </summary>
     /// <param name="blockName">block name to report</param>
     /// <returns></returns>
-    IDisposable OpenBlock(string blockName);
+    IDisposable OpenBlock([NotNull] string blockName);
+  }
+
+  /// <summary>
+  /// Introduces compilation block.
+  /// <pre>##teamcity[compilationStarted compiler='&lt;compiler name>']</pre>
+  /// and
+  /// <pre>##teamcity[compilationFinished compiler='&lt;compiler name>']</pre>
+  /// http://confluence.jetbrains.net/display/TCD7/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-ReportingCompilationMessages
+  /// </summary>
+  public interface ITeamCityCompilationBlockWriter
+  {
+    /// <summary>
+    /// Generates open compilation block. To close block call Dispose to the given handle
+    /// </summary>
+    /// <param name="compilerName"></param>
+    /// <returns></returns>
+    IDisposable OpenCompilationBlock([NotNull] string compilerName);
+  }
+
+
+  /// <summary>
+  /// Add a build log entry message
+  /// <pre>
+  /// ##teamcity[message text='&lt;message text>' errorDetails='&lt;error details>' status='&lt;status value>']
+  /// </pre>
+  /// http://confluence.jetbrains.net/display/TCD7/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-ReportingMessagesForBuildLog
+  /// </summary>
+  public interface ITeamCityMessageWriter
+  {
+    /// <summary>
+    /// Writes normal message
+    /// </summary>
+    /// <param name="text">text</param>
+    void WriteMessage([NotNull] string text);
+
+    /// <summary>
+    /// Writes warning message
+    /// </summary>
+    /// <param name="text">text</param>
+    void WriteWarning([NotNull] string text);
+
+    /// <summary>
+    /// Writes error message with details
+    /// </summary>
+    /// <param name="text">text</param>
+    /// <param name="errorDetails">error details</param>
+    void WriteError([NotNull] string text, [CanBeNull] string errorDetails);    
   }
 }
--- TeamCity.ServiceMessages/src/Write/Special/Impl/Writer/TeamCityBlockWriter.cs
+++ TeamCity.ServiceMessages/src/Write/Special/Impl/Writer/TeamCityBlockWriter.cs
@@ -1,4 +1,5 @@
 using System;
+using JetBrains.TeamCity.ServiceMessages.Annotations;
 
 namespace JetBrains.TeamCity.ServiceMessages.Write.Special.Impl.Writer
 {
@@ -12,6 +13,51 @@ namespace JetBrains.TeamCity.ServiceMessages.Write.Special.Impl.Writer
     {
       PostMessage(new SimpleServiceMessage("blockOpened"){{"name", blockName}});
       return new DisposableDelegate(() => PostMessage(new SimpleServiceMessage("blockClosed") {{"name", blockName}}));
+    }
+  }
+
+  public class TeamCityCompilationBlockWriter : BaseWriter, ITeamCityCompilationBlockWriter
+  {
+    public TeamCityCompilationBlockWriter(IServiceMessageProcessor target) : base(target)
+    {
+    }
+
+    public IDisposable OpenCompilationBlock(string compilerName)
+    {
+      PostMessage(new SimpleServiceMessage("compilationStarted") { { "compiler", compilerName } });
+      return new DisposableDelegate(() => PostMessage(new SimpleServiceMessage("compilationFinished") { { "compiler", compilerName } }));
+    }
+  }
+
+  public class TeamCityMessageWriter : BaseWriter, ITeamCityMessageWriter
+  {
+    public TeamCityMessageWriter(IServiceMessageProcessor target) : base(target)
+    {
+    }
+
+    private void Write([NotNull] string text, string details, [NotNull] string status)
+    {
+      var msg = new SimpleServiceMessage("message"){{"text", text}, {"status", status}};
+      if (!string.IsNullOrEmpty(details))
+        msg.Add("errorDetails", details);
+
+      PostMessage(msg);
+    }
+
+
+    public void WriteMessage(string text)
+    {
+      Write(text, null, "NORMAL");
+    }
+
+    public void WriteWarning(string text)
+    {
+      Write(text, null, "WARNING");
+    }
+
+    public void WriteError(string text, string errorDetails)
+    {
+      Write(text, errorDetails, "ERROR");
     }
   }
 }
