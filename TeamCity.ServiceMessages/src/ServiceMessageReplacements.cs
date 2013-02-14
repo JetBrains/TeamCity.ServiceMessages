40285db6dea0258c176c9bbdad5c27420f9a4f47
Sorin Damian <sdamian@gmail.com>
2013-02-14 14:59:54 +0200

    encode and decode unicode characters

--- TeamCity.ServiceMessages/src/ServiceMessageReplacements.cs
+++ TeamCity.ServiceMessages/src/ServiceMessageReplacements.cs
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+using System;
 using System.Text;
 using JetBrains.TeamCity.ServiceMessages.Annotations;
 
@@ -27,7 +28,7 @@ namespace JetBrains.TeamCity.ServiceMessages
     public static string Encode([NotNull] string value)
     {
       var sb = new StringBuilder(value.Length*2);
-      foreach (var ch in value)
+      foreach (char ch in value)
       {
         switch(ch)
         {
@@ -40,9 +41,21 @@ namespace JetBrains.TeamCity.ServiceMessages
           case '\u0085' : sb.Append("|x"); break;  //\u0085 (next line)=>|x
           case '\u2028' : sb.Append("|l"); break;  //\u2028 (line separator)=>|l
           case '\u2029' : sb.Append("|p"); break;  //
-          default:        sb.Append(ch); break;
+          default:
+                if (ch <= 127)
+                {
+                    sb.Append(ch); 
+                }
+                else
+                {
+                    sb.Append("|0x");
+                    sb.Append(((int) ch).ToString("x4"));
+                }
+                
+              break;
         }        
       }
+
       return sb.ToString();
     }
 
@@ -56,33 +69,55 @@ namespace JetBrains.TeamCity.ServiceMessages
     
     public static string Decode([NotNull] char[] value)
     {
-      int i = 0;
-      var sb = value;
-      var escape = false;
-      foreach (var ch in value)
-      {
-        if (!escape)
+        int i = 0;
+        char[] sb = value;
+        bool escape = false;
+        const int unicodeTokenLen = 6;
+        const int fromBase = 16;
+
+        for (int pos = 0; pos < value.Length; pos++)
         {
-          if (ch == '|') escape = true; else sb[i++] = (ch);
-        } else
-        {
-          switch (ch)
-          {
-            case '|': sb[i++] = ('|'); break;   //
-            case '\'': sb[i++] = ('\''); break;  //
-            case 'n': sb[i++] = ('\n'); break;  //
-            case 'r': sb[i++] = ('\r'); break;  //
-            case '[': sb[i++] = ('['); break;  //
-            case ']': sb[i++] = (']'); break;  //
-            case 'x': sb[i++] = ('\u0085'); break; //\u0085 (next line)=>|x
-            case 'l': sb[i++] = ('\u2028'); break;//\u2028 (line separator)=>|l
-            case 'p': sb[i++] = ('\u2029'); break; //
-            default: sb[i++] = ('?'); break; // do not thow any exception to make it faster //TODO: no exception on illegal format
-          }
-          escape = false;
-        }        
-      }
-      return new string(sb, 0, i);
+            char ch = value[pos];
+            if (!escape)
+            {
+                if (ch == '|')
+                {
+                    escape = true;
+                }
+                else
+                {
+                    sb[i++] = ch;
+                }
+            }
+            else
+            {
+                if (ch == '0' && value.Length >= pos + unicodeTokenLen && value[pos + 1] == 'x')
+                {
+                    var unicodeStr = new string(value, pos, unicodeTokenLen);
+                    sb[i++] = (char) Convert.ToInt16(unicodeStr, fromBase);
+                    pos += unicodeTokenLen-1;
+                }
+                else
+                {
+                    switch (ch)
+                    {
+                        case '|'    : sb[i++] = ('|');      break; //
+                        case '\''   : sb[i++] = ('\'');     break; //
+                        case 'n'    : sb[i++] = ('\n');     break; //
+                        case 'r'    : sb[i++] = ('\r');     break; //
+                        case '['    : sb[i++] = ('[');      break; //
+                        case ']'    : sb[i++] = (']');      break; //
+                        case 'x'    : sb[i++] = ('\u0085'); break; //\u0085 (next line)=>|x
+                        case 'l'    : sb[i++] = ('\u2028'); break; //\u2028 (line separator)=>|l
+                        case 'p'    : sb[i++] = ('\u2029'); break; //
+                        default     : sb[i++] = ('?');      break; // do not thow any exception to make it faster //TODO: no exception on illegal format
+                    }
+                }
+                escape = false;
+            }
+        }
+
+        return new string(sb, 0, i);
     }
   }
 }
