5dabd31ff57f6c8c3e6c746a9e4ae5151dd160b8
Nikolay.Pianikov <Nikolay.Pianikov@jetbrains.com>
2017-03-02 13:37:43 +0300

    fix tests

--- TeamCity.ServiceMessages/Write/Special/Impl/DefaultFlowIdGenerator.cs
+++ TeamCity.ServiceMessages/Write/Special/Impl/DefaultFlowIdGenerator.cs
@@ -25,7 +25,7 @@ namespace JetBrains.TeamCity.ServiceMessages.Write.Special.Impl
     /// </summary>
     public class DefaultFlowIdGenerator : IFlowIdGenerator
     {
-        private static int _ids;
+        private int _ids;
 
         /// <summary>
         ///     Generates new unique FlowId
