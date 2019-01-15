09da936f83ad40a84c7f2f5cb5dea0705b13c710
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2019-01-15 16:26:24 +0300

    Add samples

--- Samples/Simple/Program.cs
+++ Samples/Simple/Program.cs
@@ -1,25 +1,64 @@
 ﻿namespace Simple
 {
     using System;
+    using System.Globalization;
+    using System.IO;
     using JetBrains.TeamCity.ServiceMessages.Write.Special;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            using (var writer = new TeamCityServiceMessages().CreateWriter(str => Console.WriteLine(str)))
+            // Creating the root writer
+            using (var writer = new TeamCityServiceMessages().CreateWriter(Console.WriteLine))
+            // Creating the build log block "Tests"
             using (var block = writer.OpenBlock("Tests"))
+            // Creating the test suite "Tests"
             using (var testClass = block.OpenTestSuite("TestClass"))
             {
-                using (var test = testClass.OpenTest("Test1"))
+                // Creating the successful test
+                using (var test = testClass.OpenTest("Successful"))
                 {
                     test.WriteStdOutput("Some output");
                     test.WriteDuration(TimeSpan.FromSeconds(1));
                 }
 
-                using (var test = testClass.OpenTest("Test2"))
+                // Creating the ignored test
+                using (var test = testClass.OpenTest("Ignored"))
                 {
                     test.WriteIgnored();
+                }
+
+                // Creating the failed test
+                using (var test = testClass.OpenTest("Failed"))
+                {
+                    test.WriteFailed("Some message", "Details");
+                }
+
+                // Attaching an image to test
+                using (var test = testClass.OpenTest("Image"))
+                {
+                    writer.PublishArtifact(Path.GetFullPath("TeamCity.png") + " => TestData");
+                    test.WriteImage("TestData/TeamCity.png", "Team City Logo");
+                }
+
+                // Attaching a value to test
+                using (var test = testClass.OpenTest("Value"))
+                {
+                    test.WriteValue(1234.56.ToString(CultureInfo.InvariantCulture), "Some Value");
+                }
+
+                // Attaching a file to test
+                using (var test = testClass.OpenTest("File"))
+                {
+                    writer.PublishArtifact(Path.GetFullPath("TeamCity.png") + " => TestData");
+                    test.WriteFile("TestData/TeamCity.png", "Team City Logo file");
+                }
+
+                // Attaching a link to test
+                using (var test = testClass.OpenTest("Link"))
+                {
+                    test.WriteLink("https://www.jetbrains.com/", "JetBrains");
                 }
             }
         }
